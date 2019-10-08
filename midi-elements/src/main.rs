00d5931d107c4843df9463e24bcfd65c148b613a
Wojciech Jakobczyk <jakobczyk.woj@gmail.com>
2019-10-08 17:46:29 +0200

    Call elements engine from midi-elements
    Changes to API, linking and code necessary for first successful test run.

--- midi-elements/src/main.rs
+++ midi-elements/src/main.rs
@@ -4,16 +4,45 @@
 // pick a panicking behavior
 extern crate panic_halt; // you can put a breakpoint on `rust_begin_unwind` to catch panics
 
+use cortex_m::peripheral::Peripherals;
 use cortex_m_rt::entry;
 use cortex_m_semihosting::hprintln;
+use hal::stm32;
+use stm32f4::stm32f407::interrupt;
+use stm32f4xx_hal as hal;
+use stm32f4xx_hal::rcc::RccExt;
 
+extern crate cty;
+
+#[link(name = "elements")]
+extern "C" {
+    fn RunElements(application: bool);
+    fn Elements_DMA1_Stream5_IRQHandler();
+}
 #[entry]
 fn main() -> ! {
-    hprintln!("Hello, world!").unwrap();
+    if let (Some(p), Some(cp)) = (stm32::Peripherals::take(), Peripherals::take()) {
+        hprintln!("Hello, world!").unwrap();
 
-    // exit QEMU
-    // NOTE do not run this on hardware; it can corrupt OpenOCD state
-    // debug::exit(debug::EXIT_SUCCESS);
+        let rcc = p.RCC.constrain();
+
+        // Configure clock to 168 MHz (i.e. the maximum) and freeze it
+        let clocks = rcc
+            .cfgr
+            .sysclk(stm32f4xx_hal::time::MegaHertz(168))
+            .freeze();
+    }
+
+    unsafe {
+        RunElements(false);
+    }
 
     loop {}
+}
+
+#[interrupt]
+fn DMA1_STREAM5() {
+    unsafe {
+        Elements_DMA1_Stream5_IRQHandler();
+    }
 }
