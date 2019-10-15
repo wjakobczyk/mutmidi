c5048202899b934f3b2215241d4b656297edd295
Wojciech Jakobczyk <jakobczyk.woj@gmail.com>
2019-10-15 15:52:37 +0200

    RotaryEncoder implementation macro

--- midi-elements/src/main.rs
+++ midi-elements/src/main.rs
@@ -11,6 +11,7 @@ use embedded_hal::blocking::delay::DelayMs;
 use hal::delay::Delay;
 use hal::gpio::*;
 use hal::stm32;
+use hal::gpio::gpioe::{PE9, PE11};
 use stm32f4::stm32f407::interrupt;
 use stm32f4xx_hal as hal;
 use stm32f4xx_hal::rcc::RccExt;
@@ -24,25 +25,39 @@ extern "C" {
 }
 
 trait RotaryEncoder {
+    type PIN1;
+    type PIN2;
+
     fn read(&self) -> u32;
-    fn setup(&self);
+    fn setup(&self, pin1: Self::PIN1, pin2: Self::PIN2);
 }
 
-impl RotaryEncoder for stm32::TIM1 {
-    fn read(&self) -> u32 {
-        self.cnt.read().bits()
-    }
+macro_rules! rotary_encoder {
+    ($TIMX:ident, $PIN1X:ty, $PIN2X:ty) => {
+        impl RotaryEncoder for stm32::$TIMX {
+            type PIN1 = $PIN1X;
+            type PIN2 = $PIN2X;
 
-    fn setup(&self) {
-        self.smcr.write(|w| unsafe { w.bits(3) });
-        self.ccer.write(|w| unsafe { w.bits(0) });
-        self.arr.write(|w| unsafe { w.bits(0xFFFF) });
-        self.ccmr1_input().write(|w| unsafe { w.bits(0xC1C1) });
-        self.cnt.write(|w| unsafe { w.bits(0) });
-        self.egr.write(|w| unsafe { w.bits(0) });
-        self.cr1.write(|w| unsafe { w.bits(1) });
+            fn read(&self) -> u32 {
+                self.cnt.read().bits()
+            }
+
+            fn setup(&self, pin1: $PIN1X, pin2: $PIN2X) {
+                self.smcr.write(|w| unsafe { w.bits(3) });
+                self.ccer.write(|w| unsafe { w.bits(0) });
+                self.arr.write(|w| unsafe { w.bits(0xFFFF) });
+                self.ccmr1_input().write(|w| unsafe { w.bits(0xC1C1) });
+                self.cnt.write(|w| unsafe { w.bits(0) });
+                self.egr.write(|w| unsafe { w.bits(0) });
+                self.cr1.write(|w| unsafe { w.bits(1) });
+                pin1.internal_pull_up(true);
+                pin2.internal_pull_up(true);
+            }
+        }
     }
 }
+
+rotary_encoder!(TIM1, PE9<Alternate<AF1>>, PE11<Alternate<AF1>>);
 
 #[entry]
 fn main() -> ! {
@@ -62,9 +77,7 @@ fn main() -> ! {
 
         let gpioe = p.GPIOE.split();
 
-        gpioe.pe9.into_alternate_af1().internal_pull_up(true);
-        gpioe.pe11.into_alternate_af1().internal_pull_up(true);
-        p.TIM1.setup();
+        p.TIM1.setup(gpioe.pe9.into_alternate_af1(), gpioe.pe11.into_alternate_af1());
 
         loop {
             hprintln!("timer {}", p.TIM1.read()).unwrap();
