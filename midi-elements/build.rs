56638c47c763935bf96e4820b66ee62ffb5d2f7d
Wojciech Jakobczyk <jakobczyk.woj@gmail.com>
2019-10-22 15:01:59 +0200

    Initial bindgen integration

--- midi-elements/build.rs
+++ midi-elements/build.rs
@@ -1,3 +1,5 @@
+extern crate bindgen;
+
 use std::env;
 use std::fs::File;
 use std::io::Write;
@@ -15,4 +17,21 @@ fn main() {
     // Only re-run the build script when memory.x is changed,
     // instead of when any part of the source code changes.
     println!("cargo:rerun-if-changed=memory.x");
+
+    //does not work because generated bindings
+    //do not contain #[link(name = "elements")]
+    //potential solution in https://github.com/rust-lang/rust-bindgen/issues/1375
+    let elements_header = "../elements/elements.h";
+    println!("cargo:rerun-if-changed={}", elements_header);
+    let bindings = bindgen::Builder::default()
+        .header(elements_header)
+        .ctypes_prefix("cty")
+        .use_core()
+        .generate()
+        .expect("Unable to generate bindings");
+    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
+    bindings
+        .write_to_file(out_path.join("elements.rs"))
+        .expect("Couldn't write bindings!");
+
 }
