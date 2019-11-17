15482dda8d49eb6cb29da966981c713ec7b4f272
Wojciech Jakobczyk <jakobczyk.woj@gmail.com>
2019-11-17 20:20:22 +0100

    Bind all synth parameters in knob handler

--- midi-elements/src/elements_handlers.rs
+++ midi-elements/src/elements_handlers.rs
@@ -2,27 +2,28 @@ use alloc::boxed::Box;
 
 include!("elements.rs");
 
+#[allow(dead_code)]
 pub enum Param {
-    // ExcEnvShape,
-    // ExcBowLevel,
-    // ExcBowTimbre,
-    // ExcBlowLevel,
-    // ExcBlowMeta,
-    // ExcBlowTimbre,
+    ExcEnvShape,
+    ExcBowLevel,
+    ExcBowTimbre,
+    ExcBlowLevel,
+    ExcBlowMeta,
+    ExcBlowTimbre,
     ExcStrikeLevel,
     ExcStrikeMeta,
     ExcStrikeTimbre,
-    // ExcSignature,
-    // ResGeometry,
-    // ResBrightness,
-    // ResDamping,
-    // ResPosition,
-    // ResModulationFrequency,
-    // ResModulationOffset,
-    // ReverbDiffusion,
-    // ReverbLP,
-    // Space,
-    // ModulationFrequency,
+    ExcSignature,
+    ResGeometry,
+    ResBrightness,
+    ResDamping,
+    ResPosition,
+    ResModulationFrequency,
+    ResModulationOffset,
+    ReverbDiffusion,
+    ReverbLP,
+    Space,
+    ModulationFrequency,
 }
 
 const KNOB_SCALER: f32 = 20f32;
@@ -54,8 +55,25 @@ macro_rules! param_bind {
 
 pub fn create_knob_handler(param: Param) -> Box<dyn FnMut(i8) -> u8> {
     match param {
+        Param::ExcEnvShape => param_bind!(exciter_envelope_shape),
+        Param::ExcBowLevel => param_bind!(exciter_bow_level),
+        Param::ExcBowTimbre => param_bind!(exciter_bow_timbre),
+        Param::ExcBlowLevel => param_bind!(exciter_blow_level),
+        Param::ExcBlowMeta => param_bind!(exciter_blow_meta),
+        Param::ExcBlowTimbre => param_bind!(exciter_blow_timbre),
         Param::ExcStrikeLevel => param_bind!(exciter_strike_level),
         Param::ExcStrikeMeta => param_bind!(exciter_strike_meta),
         Param::ExcStrikeTimbre => param_bind!(exciter_strike_timbre),
+        Param::ExcSignature => param_bind!(exciter_signature),
+        Param::ResGeometry => param_bind!(resonator_geometry),
+        Param::ResBrightness => param_bind!(resonator_brightness),
+        Param::ResDamping => param_bind!(resonator_damping),
+        Param::ResPosition => param_bind!(resonator_position),
+        Param::ResModulationFrequency => param_bind!(resonator_modulation_frequency),
+        Param::ResModulationOffset => param_bind!(resonator_modulation_offset),
+        Param::ReverbDiffusion => param_bind!(reverb_diffusion),
+        Param::ReverbLP => param_bind!(reverb_lp),
+        Param::Space => param_bind!(space),
+        Param::ModulationFrequency => param_bind!(modulation_frequency),
     }
 }
